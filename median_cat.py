7755fb014f676cd555ef7cf86e0ff4645cf1cfac
MHendriF <hendrifebriansyah28@gmail.com>
2024-01-17 15:54:57 +0700

    mencari nilai mean dan median

--- median_cat.py
+++ median_cat.py
@@ -1,5 +1,5 @@
 import numpy as np
     
 jumlah_kucing = np.array([3, 2, 1, 1, 2, 3, 2, 1, 0, 2])
-mean = jumlah_kucing.mean()
-print("Nilai mean:", mean)
+nilai_median = np.median(jumlah_kucing)
+print("Nilai median:", nilai_median)
